74db8d38d88e77180caf4aea0674dab4eda2e4be
holyblaz <98361526+holyblaz@users.noreply.github.com>
2022-04-29 15:42:16 +0300

    Update CreditPaymentService.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -3,8 +3,8 @@ public class CreditPaymentService {
         int months = -(loanTerm * 12);
         float monthInterestRate = interestRate / (100 * 12); // Рассчет процентной ставки в месяц
 
-        float value_1 = 1 + monthInterestRate;               //Переменные расчета для подставления в общую формулу
-        double value_2 = Math.pow(value_1, months);
+        float value1 = 1 + monthInterestRate;               //Переменные расчета для подставления в общую формулу
+        double value2 = Math.pow(value_1, months);
 
         int monthlyPay = (int) (loanAmount * (monthInterestRate / (1 - value_2))); // формула рассчета аннуитета
 
